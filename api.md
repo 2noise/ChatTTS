858cf2e129a87e550419aac741ff4cdc0c972fa6
CyberWon <mail@shuaibo.wang>
2024-05-28 13:47:56 +0800

    ChatTTS HTTP API

---
+++ api.md
@@ -0,0 +1,26 @@
+# API使用说明
+
+API是使用FastAPI开发的，目前没考虑并发，在4090 没使用flash attn推理，速度在80左右。
+
+# 环境要求
+python: 3.9+1
+torch: 2.2
+transformers: 4.41
+
+
+# 安装
+```bash
+pip install -r requirements.txt
+# 需要自己提前下载 https://huggingface.co/2Noise/ChatTTS
+python api.py --model-dir "本地权重路径"
+```
+
+# 使用
+```
+curl http://localhost:12456?text=你好 -o output.wav
+```
+
+# 已知问题
+1. 文本过长会有问题，暂时不要超过100个字。
+2. 不支持并发，请不要尝试
+3. 采样率为24000，有一些软件用起来会有问题
---
+++ api.py
@@ -0,0 +1,73 @@
+import io
+import os
+import wave
+import numpy as np
+from fastapi import FastAPI, Query, Depends
+from fastapi.responses import StreamingResponse, Response
+
+from omegaconf import OmegaConf
+from pydantic import BaseModel
+
+
+import ChatTTS
+import uvicorn
+
+import argparse
+from loguru import logger
+
+
+class TTS(BaseModel):
+    """TTS GET/POST request"""
+    text: str = Query("欢迎使用ChatTTS API", description="text to synthesize")
+
+
+class Chat(ChatTTS.Chat):
+    """重写一下load_models方法，方便我们自定义models的路径，照顾国内的宝宝"""
+
+    def load_models(self, source=''):
+        download_path = source
+        self._load(**{k: os.path.join(download_path, v) for k, v in
+                      OmegaConf.load(os.path.join(download_path, 'config', 'path.yaml')).items()})
+
+
+app = FastAPI()
+
+chat = Chat()
+
+
+def wave_header_chunk(frame_input=b"", channels=1, sample_width=2, sample_rate=24000):
+    wav_buf = io.BytesIO()
+    with wave.open(wav_buf, "wb") as vfout:
+        vfout.setnchannels(channels)
+        vfout.setsampwidth(sample_width)
+        vfout.setframerate(sample_rate)
+        vfout.writeframes(frame_input)
+    wav_buf.seek(0)
+    return wav_buf.read()
+
+
+def infer(text):
+    chunks = (chat.infer([text], use_decoder=True)[0] * 32768).astype(np.int16)
+    yield wave_header_chunk()
+    for chunk in chunks:
+        if chunk is not None:
+            chunk = chunk.tobytes()
+            yield chunk
+
+
+@app.get("/")
+async def index(params: TTS = Depends(TTS)):
+    logger.debug(params)
+    wavs = infer(params.text)
+    return StreamingResponse(wavs, media_type="audio/wav")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", type=str, default="0.0.0.0")
+    parser.add_argument("--port", type=int, default=12456)
+    parser.add_argument("--model-dir", type=str, default="./models")
+    args = parser.parse_args()
+    logger.debug(f"model_dir: {args.model_dir}")
+    chat.load_models(args.model_dir)
+    uvicorn.run(app, host="0.0.0.0", port=12456)
