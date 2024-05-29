095f8946b8a3d981461b911b1217dec9ec3dcb80
Vw5SblVT <luoyonghao2018@gmail.com>
2024-05-29 23:07:38 +0800

    Feat: add MPS support

--- ChatTTS/utils/gpu_utils.py
+++ ChatTTS/utils/gpu_utils.py
@@ -1,10 +1,13 @@
-
 import torch
 import logging
 
-def select_device(min_memory = 2048):
+def select_device(min_memory=2048):
     logger = logging.getLogger(__name__)
-    if torch.cuda.is_available():
+    
+    if torch.backends.mps.is_available():
+        device = torch.device('mps')
+        logger.log(logging.INFO, 'Using MPS (Apple Silicon) as the device.')
+    elif torch.cuda.is_available():
         available_gpus = []
         for i in range(torch.cuda.device_count()):
             props = torch.cuda.get_device_properties(i)
@@ -17,7 +20,7 @@ def select_device(min_memory = 2048):
             logger.log(logging.WARNING, f'GPU {selected_gpu} has {round(free_memory_mb, 2)} MB memory left.')
             device = torch.device('cpu')
     else:
-        logger.log(logging.WARNING, f'No GPU found, use CPU instead')
+        logger.log(logging.WARNING, 'No GPU found, use CPU instead')
         device = torch.device('cpu')
     
     return device
