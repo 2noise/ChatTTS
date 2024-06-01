31cab5b92e764aa85298bf1123a5b0214968db0e
王华侨 <honestqiao@163.com>
2024-06-02 02:01:06 +0800

    Add preset voices config

--- webui.py
+++ webui.py
@@ -8,6 +8,20 @@ import numpy as np
 
 import ChatTTS
 
+# 音色选项：用于预置合适的音色
+voices = {
+    "默认": {"seed": 2},
+    "音色1": {"seed": 1111},
+    "音色2": {"seed": 2222},
+    "音色3": {"seed": 3333},
+    "音色4": {"seed": 4444},
+    "音色5": {"seed": 5555},
+    "音色6": {"seed": 6666},
+    "音色7": {"seed": 7777},
+    "音色8": {"seed": 8888},
+    "音色9": {"seed": 9999},
+    "音色10": {"seed": 11111},
+}
 
 def generate_seed():
     new_seed = random.randint(1, 100000000)
@@ -16,35 +30,38 @@ def generate_seed():
         "value": new_seed
         }
 
+# 返回选择音色对应的seed
+def on_voice_change(vocie_selection):
+    return voices.get(vocie_selection)['seed']
 
 def generate_audio(text, temperature, top_P, top_K, audio_seed_input, text_seed_input, refine_text_flag):
 
     torch.manual_seed(audio_seed_input)
     rand_spk = chat.sample_random_speaker()
     params_infer_code = {
-        'spk_emb': rand_spk, 
+        'spk_emb': rand_spk,
         'temperature': temperature,
         'top_P': top_P,
         'top_K': top_K,
         }
     params_refine_text = {'prompt': '[oral_2][laugh_0][break_6]'}
-    
+
     torch.manual_seed(text_seed_input)
 
     if refine_text_flag:
-        text = chat.infer(text, 
+        text = chat.infer(text,
                           skip_refine_text=False,
                           refine_text_only=True,
                           params_refine_text=params_refine_text,
                           params_infer_code=params_infer_code
                           )
-    
-    wav = chat.infer(text, 
-                     skip_refine_text=True, 
-                     params_refine_text=params_refine_text, 
+
+    wav = chat.infer(text,
+                     skip_refine_text=True,
+                     params_refine_text=params_refine_text,
                      params_infer_code=params_infer_code
                      )
-    
+
     audio_data = np.array(wav[0]).flatten()
     sample_rate = 24000
     text_data = text[0] if isinstance(text, list) else text
@@ -58,7 +75,7 @@ def main():
         gr.Markdown("# ChatTTS Webui")
         gr.Markdown("ChatTTS Model: [2noise/ChatTTS](https://github.com/2noise/ChatTTS)")
 
-        default_text = "四川美食确实以辣闻名，但也有不辣的选择。比如甜水面、赖汤圆、蛋烘糕、叶儿粑等，这些小吃口味温和，甜而不腻，也很受欢迎。"        
+        default_text = "四川美食确实以辣闻名，但也有不辣的选择。[uv_break]比如甜水面、赖汤圆、蛋烘糕、叶儿粑等，这些小吃口味温和，甜而不腻，也很受欢迎。[laugh]"
         text_input = gr.Textbox(label="Input Text", lines=4, placeholder="Please Input Text...", value=default_text)
 
         with gr.Row():
@@ -68,26 +85,31 @@ def main():
             top_k_slider = gr.Slider(minimum=1, maximum=20, step=1, value=20, label="top_K")
 
         with gr.Row():
+            voice_options = {}
+            voice_selection = gr.Dropdown(label="音色", choices=voices.keys(), value='默认')
             audio_seed_input = gr.Number(value=2, label="Audio Seed")
             generate_audio_seed = gr.Button("\U0001F3B2")
             text_seed_input = gr.Number(value=42, label="Text Seed")
             generate_text_seed = gr.Button("\U0001F3B2")
 
         generate_button = gr.Button("Generate")
-        
+
         text_output = gr.Textbox(label="Output Text", interactive=False)
         audio_output = gr.Audio(label="Output Audio")
 
-        generate_audio_seed.click(generate_seed, 
-                                  inputs=[], 
+        # 使用Gradio的回调功能来更新数值输入框
+        voice_selection.change(fn=on_voice_change, inputs=voice_selection, outputs=audio_seed_input)
+
+        generate_audio_seed.click(generate_seed,
+                                  inputs=[],
                                   outputs=audio_seed_input)
-        
-        generate_text_seed.click(generate_seed, 
-                                 inputs=[], 
+
+        generate_text_seed.click(generate_seed,
+                                 inputs=[],
                                  outputs=text_seed_input)
-        
-        generate_button.click(generate_audio, 
-                              inputs=[text_input, temperature_slider, top_p_slider, top_k_slider, audio_seed_input, text_seed_input, refine_text_checkbox], 
+
+        generate_button.click(generate_audio,
+                              inputs=[text_input, temperature_slider, top_p_slider, top_k_slider, audio_seed_input, text_seed_input, refine_text_checkbox],
                               outputs=[audio_output, text_output])
 
     parser = argparse.ArgumentParser(description='ChatTTS demo Launch')
