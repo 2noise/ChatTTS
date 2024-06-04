1ccc0d560ea36b0b6113ddf409923c0913f81744
ain-soph <ain-soph@live.com>
2024-06-04 15:21:42 -0400

    add finetune

---
+++ finetune.py
@@ -0,0 +1,215 @@
+import argparse
+from enum import StrEnum
+
+import torch.utils.data
+import torch.nn
+import transformers
+
+import ChatTTS
+import ChatTTS.model.gpt
+import ChatTTS.model.dvae
+from utils.dataset import XzListFolder, AudioFolder, AudioCollator
+
+
+class TrainModule(StrEnum):
+    GPT_SPEAKER = 'gpt_speaker'
+    GPT = 'gpt'
+    SPEAKER = 'speaker'
+
+    AUTOENCODER = 'autoencoder'
+    ENCODER = 'encoder'
+    DECODER = 'decoder'
+
+
+def train_autoencoder(chat: ChatTTS.Chat, dataset: AudioFolder, train_module: TrainModule = TrainModule.AUTOENCODER):
+    tokenizer: transformers.PreTrainedTokenizer = chat.pretrain_models['tokenizer']
+    # encoder: ChatTTS.model.dvae.DVAE = chat.pretrain_models['decoder']   # TODO: placeholder
+    decoder: ChatTTS.model.dvae.DVAE = chat.pretrain_models['decoder']
+
+    train_params = list(decoder.parameters())   # TODO: placeholder
+    # match train_module:
+    #     case TrainModule.AUTOENCODER:
+    #         encoder.train().requires_grad_()
+    #         decoder.train().requires_grad_()
+    #         train_params = list(encoder.parameters()) + list(decoder.parameters())
+    #     case TrainModule.ENCODER:
+    #         encoder.train().requires_grad_()
+    #         decoder.eval().requires_grad_(False)
+    #         train_params = list(encoder.parameters())
+    #     case TrainModule.DECODER:
+    #         encoder.eval().requires_grad_(False)
+    #         decoder.train().requires_grad_()
+    #         train_params = list(decoder.parameters())
+
+    loss_fn = torch.nn.MSELoss()
+    optimizer = torch.optim.Adam(train_params, lr=1e-4)
+    lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 10, 1e-6)
+
+    loader = torch.utils.data.DataLoader(dataset, batch_size=16, shuffle=True, collate_fn=AudioCollator(text_pad=tokenizer.pad_token_id))
+    for epoch in range(10):
+        for batch in loader:
+            audio_mel_spec: torch.Tensor = batch['audio_mel_spec']  # (batch_size, audio_len, 100)
+
+            batch_size = audio_mel_spec.size(0)
+            audio_len = audio_mel_spec.size(1)
+
+            # TODO: placeholder
+            # audio_quantized_latents, audio_input_ids = encode(audio_mel_spec)
+            audio_quantized_latents = torch.zeros(
+                (batch_size, audio_len, decoder.out_conv.in_channels*2),
+                dtype=audio_mel_spec.dtype,
+                device=audio_mel_spec.device,
+            )   # (batch_size, audio_len, audio_dim)
+
+            gen_mel_spec = decoder(audio_quantized_latents)
+            loss: torch.Tensor = loss_fn(gen_mel_spec, audio_mel_spec)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        lr_scheduler.step()
+    optimizer.zero_grad()
+
+
+def train_gpt(chat: ChatTTS.Chat, dataset: AudioFolder, train_module: TrainModule = TrainModule.GPT_SPEAKER):
+    tokenizer: transformers.PreTrainedTokenizer = chat.pretrain_models['tokenizer']
+    gpt: ChatTTS.model.gpt.GPT_warpper = chat.pretrain_models['gpt']
+    # encoder: ChatTTS.model.dvae.DVAE = chat.pretrain_models['decoder']   # TODO: placeholder
+    decoder: ChatTTS.model.dvae.DVAE = chat.pretrain_models['decoder']   # TODO: placeholder
+
+    # encoder.eval().requires_grad_(False)
+    if train_module == TrainModule.SPEAKER:
+        gpt.eval().requires_grad_(False)
+    else:
+        gpt.train().requires_grad_()
+
+    speaker_embeds = {
+        speaker: torch.randn(
+            768,
+            device=dataset.device,
+            requires_grad=train_module in [TrainModule.GPT_SPEAKER, TrainModule.SPEAKER],
+        ) for speaker in dataset.speakers
+    }
+    SPEAKER_TOKEN: int = tokenizer.convert_tokens_to_ids('[spk_emb]')
+
+    loss_fn = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(gpt.parameters(), lr=1e-4)
+    lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 10, 1e-6)
+
+    loader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True, collate_fn=AudioCollator(text_pad=tokenizer.pad_token_id))
+    for epoch in range(10):
+        for batch in loader:
+            print('hello')
+            speakers: list[str] = batch['speaker']  # (batch_size,)
+            text_input_ids: torch.Tensor = batch['text_input_ids']   # (batch_size, text_len)
+            text_attention_mask: torch.Tensor = batch['text_attention_mask']   # (batch_size, text_len)
+            audio_mel_spec: torch.Tensor = batch['audio_mel_spec']   # (batch_size, audio_len, 100)
+            audio_attention_mask: torch.Tensor = batch['audio_attention_mask']   # (batch_size, audio_len)
+
+            batch_size = text_attention_mask.size(0)
+            text_len = text_attention_mask.size(1)
+            audio_len = audio_attention_mask.size(1)
+
+            # TODO: placeholder
+            # audio_quantized_latents, audio_input_ids = encode(audio_mel_spec)
+            audio_input_ids = torch.zeros(
+                (batch_size, audio_len, gpt.num_vq),
+                dtype=text_input_ids.dtype,
+                device=text_input_ids.device,
+            )   # (batch_size, audio_len, num_vq)
+            audio_quantized_latents = torch.zeros(
+                (batch_size, audio_len, decoder.out_conv.in_channels*2),
+                dtype=audio_mel_spec.dtype,
+                device=audio_mel_spec.device,
+            )   # (batch_size, audio_len, audio_dim)
+
+            input_ids = torch.cat(   # (batch_size, text_len + audio_len, num_vq)
+                [
+                    text_input_ids.unsqueeze(-1).repeat(1, 1, gpt.num_vq),   # (batch_size, text_len, num_vq)
+                    audio_input_ids,   # (batch_size, audio_len, num_vq)
+                ],
+                dim=1,
+            )
+            attention_mask = torch.cat(   # (batch_size, text_len + audio_len)
+                [text_attention_mask, audio_attention_mask],
+                dim=1,
+            )
+            text_mask = torch.cat(   # (batch_size, text_len + audio_len)
+                [
+                    torch.ones_like(text_attention_mask, dtype=bool),
+                    torch.zeros_like(audio_attention_mask, dtype=bool),
+                ],
+                dim=1,
+            )
+
+            # (batch_size, text_len + audio_len, 768)
+            inputs_embeds = gpt.get_emb(input_ids=input_ids, text_mask=text_mask)
+
+            # (batch_size, text_len + audio_len)
+            indices = torch.all(input_ids == SPEAKER_TOKEN, dim=-1)
+            for i, speaker in enumerate(speakers):
+                inputs_embeds[i, indices[i]] = torch.nn.functional.normalize(
+                    speaker_embeds[speaker].to(dtype=inputs_embeds.dtype).unsqueeze(0),
+                    p=2.0,
+                    dim=1,
+                    eps=1e-12,
+                )
+            outputs = gpt.gpt.forward(inputs_embeds=inputs_embeds, attention_mask=attention_mask)
+            hidden_states = outputs.last_hidden_state  # (batch_size, text_len + audio_len, 768)
+            text_hidden_states = hidden_states[:, :text_len-1]  # (batch_size, text_len-1, 768)
+            audio_hidden_states = hidden_states[:, text_len-1:-1]  # (batch_size, audio_len, 768)
+
+            text_logits: torch.Tensor = gpt.head_text(text_hidden_states)  # (batch_size, text_len-1, num_class_text)
+            audio_logits = torch.stack(
+                [gpt.head_code[i](audio_hidden_states) for i in range(gpt.num_vq)],
+                dim=3,
+            )  # (batch_size, audio_len, num_class_audio)
+
+            text_loss: torch.Tensor = loss_fn(text_logits.flatten(0, 1), text_input_ids[:, 1:].flatten(0, 1))
+            audio_loss: torch.Tensor = loss_fn(audio_logits.flatten(0, 1), audio_input_ids.flatten(0, 1))
+            loss = text_loss + audio_loss
+            if False:   # TODO: placeholder
+                loss += torch.nn.functional.mse_loss(
+                    audio_hidden_states,
+                    audio_quantized_latents,
+                )
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        lr_scheduler.step()
+    optimizer.zero_grad()
+
+
+def main():
+    parser = argparse.ArgumentParser(description='ChatTTS demo Launch')
+    parser.add_argument('--local_path', type=str, default=None, help='the local_path if need')
+    parser.add_argument('--data_path', type=str, default='dummy_data/data.json', help='the data_path')
+    parser.add_argument(
+        '--train_module', type=str, default='gpt',
+        choices=['gpt_speaker', 'gpt', 'speaker', 'autoencoder', 'encoder', 'decoder'],
+    )
+    args = parser.parse_args()
+    local_path: str | None = args.local_path
+    data_path: str = args.data_path
+    train_module: TrainModule = args.train_module
+
+    chat = ChatTTS.Chat()
+    if local_path is None:
+        chat.load_models()
+    else:
+        print('local model path:', local_path)
+        chat.load_models('local', local_path=local_path)
+
+    dataset = XzListFolder(
+        root=data_path,
+        tokenizer=chat.pretrain_models['tokenizer'],
+        vocos_model=chat.pretrain_models['vocos'],
+        # device=None,
+        # speakers=None,  # set(['speaker_A', 'speaker_B'])
+    )
+    train = train_gpt if train_module in [TrainModule.SPEAKER, TrainModule.GPT, TrainModule.GPT_SPEAKER] else train_autoencoder
+    train(chat=chat, dataset=dataset, train_module=train_module)
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils/dataset.py
@@ -0,0 +1,261 @@
+import os
+import functools
+import json
+import logging
+import abc
+import typing
+
+import torch.utils.data
+import torchaudio
+import transformers
+import vocos
+
+from ChatTTS.utils.infer_utils import count_invalid_characters, detect_language, apply_character_map, apply_half2full_map
+
+
+class LazyDataType(typing.TypedDict):
+    filepath: str
+    speaker: str
+    lang: str
+    text: str
+
+
+class DataType(LazyDataType):
+    text_input_ids: torch.Tensor
+    text_attention_mask: torch.Tensor
+    audio_mel_spec: torch.Tensor
+    audio_attention_mask: torch.Tensor
+
+
+class AudioFolder(torch.utils.data.Dataset, abc.ABC):
+    def __init__(
+        self,
+        root: str,
+        tokenizer: transformers.PreTrainedTokenizer,
+        vocos_model: vocos.Vocos,
+        device: str | torch.device | None = None,
+        speakers: typing.Iterable[str] | None = None,
+        default_speaker: str = None,
+        default_lang: str = None,
+    ) -> None:
+        self.default_speaker = default_speaker
+        self.default_lang = default_lang
+
+        self.logger = logging.getLogger(__name__)
+        self.normalizer = {}
+
+        self.tokenizer = tokenizer
+        self.vocos = vocos_model
+        self.device = device or next(self.vocos.parameters()).device
+
+        self.lazy_data, self.speakers = self.get_lazy_data(root, speakers)
+
+        self.text_input_ids = [self.preprocess_text(item['text'], item['lang']) for item in self.lazy_data]
+        self.audio_mel_specs = [self.preprocess_audio(item['filepath']) for item in self.lazy_data]
+        # TODO: mel_specs (100, seq_len), seq_len is different for audio samples.
+
+    @abc.abstractmethod
+    def get_raw_data(self, root: str) -> list[dict[str, str]]:
+        ...
+
+    def __len__(self):
+        return len(self.lazy_data)
+
+    def __getitem__(self, n: int) -> DataType:
+        text_input_ids = self.text_input_ids[n]
+        audio_mel_spec = self.audio_mel_specs[n]
+        text_attention_mask = torch.ones(len(text_input_ids), device=text_input_ids.device)
+        audio_attention_mask = torch.ones(len(audio_mel_spec), device=audio_mel_spec.device)
+        return {
+            'filepath': self.lazy_data[n]['filepath'],
+            'speaker': self.lazy_data[n]['speaker'],
+            'lang': self.lazy_data[n]['lang'],
+            'text': self.lazy_data[n]['text'],
+            'text_input_ids': text_input_ids,
+            'text_attention_mask': text_attention_mask,
+            'audio_mel_spec': audio_mel_spec,
+            'audio_attention_mask': audio_attention_mask,
+        }
+
+    def get_lazy_data(
+        self,
+        root: str,
+        speakers: typing.Iterable[str] | None = None,
+    ) -> tuple[list[LazyDataType], set[str]]:
+        if speakers is not None:
+            new_speakers = set(speakers)
+        else:
+            new_speakers = set()
+        lazy_data = []
+
+        raw_data = self.get_raw_data(root)
+        folder_path = os.path.dirname(root)
+        for item in raw_data:
+            if 'speaker' not in item:
+                item['speaker'] = self.default_speaker
+            if 'lang' not in item:
+                item['lang'] = self.default_lang
+
+            if speakers is not None and item['speaker'] not in speakers:
+                continue
+            if speakers is None and item['speaker'] not in new_speakers:
+                new_speakers.add(item['speaker'])
+            lazy_data.append({
+                'filepath': os.path.join(folder_path, item['filepath']),
+                'speaker': item['speaker'],
+                'lang': item['lang'].lower(),
+                'text': item['text'],
+            })
+        return lazy_data, new_speakers
+
+    def preprocess_text(
+        self,
+        text: str,
+        lang: str,
+        do_text_normalization: bool = True,
+    ) -> torch.Tensor:
+        if do_text_normalization:
+            _lang = lang or detect_language(text)
+            self.init_normalizer(_lang)
+            text = self.normalizer[_lang](text)
+            if _lang == 'zh':
+                text = apply_half2full_map(text)
+
+        invalid_characters = count_invalid_characters(text)
+        if len(invalid_characters):
+            self.logger.log(logging.WARNING, f'Invalid characters found! : {invalid_characters}')
+            text = apply_character_map(text)
+
+        # if not skip_refine_text:
+        #     text_tokens = refine_text(self.pretrain_models, text, **params_refine_text)['ids']
+        #     text_tokens = [i[i < self.pretrain_models['tokenizer'].convert_tokens_to_ids('[break_0]')] for i in text_tokens]
+        #     text = self.pretrain_models['tokenizer'].batch_decode(text_tokens)
+        #     if refine_text_only:
+        #         return text
+
+        text = f'[Stts][spk_emb]{text}[Ptts]'
+        # text = f'[Stts][empty_spk]{text}[Ptts]'
+
+        text_token = self.tokenizer(text, return_tensors='pt', add_special_tokens=False).to(device=self.device)
+        return text_token['input_ids']
+
+    def preprocess_audio(self, filepath: str) -> torch.Tensor:
+        waveform, sample_rate = torchaudio.load(filepath)
+        if sample_rate != 24_000:
+            waveform = torchaudio.functional.resample(waveform, orig_freq=sample_rate, new_freq=24_000)
+        mel_spec: torch.Tensor = self.vocos.feature_extractor(waveform.to(device=self.device))
+        return mel_spec.squeeze(0).transpose(0, 1)  # (audio_len, 100)
+
+    def init_normalizer(self, lang: str):
+        if lang not in self.normalizer:
+            if lang == 'zh':
+                from tn.chinese.normalizer import Normalizer
+                self.normalizer[lang] = Normalizer().normalize
+            else:
+                from nemo_text_processing.text_normalization.normalize import Normalizer
+                self.normalizer[lang] = functools.partial(
+                    Normalizer(input_case='cased', lang=lang).normalize,
+                    verbose=False,
+                    punct_post_process=True,
+                )
+
+
+class JsonFolder(AudioFolder):
+    def get_raw_data(self, root: str) -> list[dict[str, str]]:
+        with open(root, 'r', encoding='utf-8') as f:
+            raw_data = json.load(f)
+        return raw_data
+
+
+class ListFolder(AudioFolder):
+    def get_raw_data(self, root: str) -> list[dict[str, str]]:
+        raw_data = []
+        with open(os.path.join(root), 'r', encoding='utf-8') as f:
+            for line in f.readlines():
+                filepath, speaker, lang, text = line.split(sep='|', maxsplit=3)
+                text = text.strip().removesuffix('\n')
+                raw_data.append({
+                    'text': text,
+                    'filepath': filepath,
+                    'speaker': speaker,
+                    'lang': lang,
+                })
+        return raw_data
+
+
+class XzListFolder(ListFolder):
+    """[Xz乔希](https://space.bilibili.com/5859321)"""
+
+    def get_raw_data(self, root: str) -> list[dict[str, str]]:
+        raw_data = super().get_raw_data(root)
+        for item in raw_data:
+            item['filepath'] = os.path.join(
+                os.path.basename(root).removesuffix('.list'),
+                os.path.basename(item['filepath']),
+            )
+        return raw_data
+
+
+class AudioCollator:
+    def __init__(self, text_pad: int = 0, audio_pad: int = 0):
+        self.text_pad = text_pad
+        self.audio_pad = audio_pad
+
+    def __call__(self, batch: list[DataType]):
+        batch = [x for x in batch if x is not None]
+
+        audio_maxlen = max(len(item['audio_attention_mask']) for item in batch)
+        text_maxlen = max(len(item['text_attention_mask']) for item in batch)
+
+        file_path = []
+        speaker = []
+        lang = []
+        text = []
+        text_input_ids = []
+        text_attention_mask = []
+        audio_mel_spec = []
+        audio_attention_mask = []
+
+        for x in batch:
+            file_path.append(x['filepath'])
+            speaker.append(x['speaker'])
+            lang.append(x['lang'])
+            text.append(x['text'])
+            text_input_ids.append(
+                torch.nn.functional.pad(
+                    x['text_input_ids'],
+                    (text_maxlen - len(x['text_input_ids']), 0),
+                    value=self.text_pad,
+                )
+            )
+            text_attention_mask.append(
+                torch.nn.functional.pad(
+                    x['text_attention_mask'],
+                    (text_maxlen - len(x['text_attention_mask']), 0),
+                    value=0,
+                )
+            )
+            audio_mel_spec.append(
+                torch.nn.functional.pad(
+                    x['audio_mel_spec'],
+                    (0, audio_maxlen - len(x['audio_mel_spec'])),
+                    value=self.audio_pad,
+                )
+            )
+            audio_attention_mask.append(
+                torch.nn.functional.pad(
+                    x['audio_attention_mask'],
+                    (0, audio_maxlen - len(x['audio_attention_mask'])),
+                    value=0,
+                )
+            )
+        return {
+            'filepath': file_path,
+            'speaker': speaker,
+            'lang': lang,
+            'text': text,
+            'text_input_ids': torch.stack(text_input_ids),
+            'text_attention_mask': torch.stack(text_attention_mask),
+            'audio_mel_spec': torch.stack(audio_mel_spec),
+            'audio_attention_mask': torch.stack(audio_attention_mask),
+        }
