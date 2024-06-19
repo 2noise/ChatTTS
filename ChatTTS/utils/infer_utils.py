78ffe751cd73e9ba59bfbaae10cf817a0c800ef5
6drf21e <nir311@163.com>
2024-06-19 15:08:46 +0800

    [Refactor] document process in class docstring.

--- ChatTTS/utils/infer_utils.py
+++ ChatTTS/utils/infer_utils.py
@@ -48,27 +48,27 @@ class CustomRepetitionPenaltyLogitsProcessor():
         return scores
 
 class HomophonesReplacer:
+    """
+    Homophones Replacer
+
+    Replace the mispronounced characters with correctly pronounced ones.
+
+    Creation process of homophones_map.json:
+
+    1. Establish a word corpus using the [Tencent AI Lab Embedding Corpora v0.2.0 large] with 12 million entries. After cleaning, approximately 1.8 million entries remain. Use ChatTTS to infer the text.
+    2. Record discrepancies between the inferred and input text, identifying about 180,000 misread words.
+    3. Create a pinyin to common characters mapping using correctly read characters by ChatTTS.
+    4. For each discrepancy, extract the correct pinyin using [python-pinyin] and find homophones with the correct pronunciation from the mapping.
+
+    Thanks to:
+    [Tencent AI Lab Embedding Corpora for Chinese and English Words and Phrases](https://ai.tencent.com/ailab/nlp/en/embedding.html)
+    [python-pinyin](https://github.com/mozillazg/python-pinyin)
+
+    """
     def __init__(self, map_file_path):
         self.homophones_map = self.load_homophones_map(map_file_path)
 
     def load_homophones_map(self, map_file_path):
-        """
-        Load homophones map from a JSON file.
-
-        homophones map creation process:
-
-        1. Establish a word corpus using the [Tencent AI Lab Embedding Corpora v0.2.0 large] with 12 million entries. After cleaning, approximately 1.8 million entries remain. Use ChatTTS to infer the text.
-        2. Record discrepancies between the inferred and input text, identifying about 180,000 misread words.
-        3. Create a pinyin to common characters mapping using correctly read characters by ChatTTS.
-        4. For each discrepancy, extract the correct pinyin using [python-pinyin] and find homophones with the correct pronunciation from the mapping.
-
-        Thanks to:
-        [Tencent AI Lab Embedding Corpora for Chinese and English Words and Phrases](https://ai.tencent.com/ailab/nlp/en/embedding.html)
-        [python-pinyin](https://github.com/mozillazg/python-pinyin)
-
-        :param map_file_path: path to the homophones map file
-        :return:
-        """
         with open(map_file_path, 'r', encoding='utf-8') as f:
             homophones_map = json.load(f)
         return homophones_map
