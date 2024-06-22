f5d9e22b5fc3fc4681b1f56914a76c28a7805660
源文雨 <41315874+fumiama@users.noreply.github.com>
2024-06-22 23:50:02 +0900

    optimize(gpt): apply type definitions

--- ChatTTS/model/dvae.py
+++ ChatTTS/model/dvae.py
@@ -1,8 +1,8 @@
 import math
 from typing import List, Optional
 
-import pybase16384 as b14
 import numpy as np
+import pybase16384 as b14
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
