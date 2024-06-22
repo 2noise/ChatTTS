17723310bf60a1f6cf9392622b8e402fa4a645a7
源文雨 <41315874+fumiama@users.noreply.github.com>
2024-06-22 15:14:28 +0900

    resolve conflict

--- examples/cmd/run.py
+++ examples/cmd/run.py
@@ -6,65 +6,49 @@ if sys.platform == "darwin":
 now_dir = os.getcwd()
 sys.path.append(now_dir)
 
+import wave
+import argparse
+
 from dotenv import load_dotenv
 load_dotenv("sha256.env")
 
-import wave
 import ChatTTS
-from IPython.display import Audio
-import numpy as np
-import argparse
 
+from tools.audio import unsafe_float_to_int16
+from tools.logger import get_logger
+
+logger = get_logger("Command")
 
 def save_wav_file(wav, index):
     wav_filename = f"output_audio_{index}.wav"
-    # Convert numpy array to bytes and write to WAV file
-    wav_bytes = (wav * 32768).astype('int16').tobytes()
     with wave.open(wav_filename, "wb") as wf:
         wf.setnchannels(1)  # Mono channel
         wf.setsampwidth(2)  # Sample width in bytes
         wf.setframerate(24000)  # Sample rate in Hz
-        wf.writeframes(wav_bytes)
-    print(f"Audio saved to {wav_filename}")
+        wf.writeframes(unsafe_float_to_int16(wav))
+    logger.info(f"Audio saved to {wav_filename}")
 
-def main(text="<YOUR TEXT HERE>", stream=False):
-    print(f"{stream=} Received text input: {text}")
+def main(texts: list[str]):
+    logger.info("Text input: %s", str(texts))
 
-    chat = ChatTTS.Chat()
-    print("Initializing ChatTTS...")
-    # if using macbook(M1), I suggest you set `device='cpu', compile=False`
-    chat.load_models()
-    print("Models loaded successfully.")
-
-    texts = [text]
-    print("Text prepared for inference:", texts)
-
-    wavs_gen = chat.infer(texts, use_decoder=True, stream=stream)
-    print("Inference completed. Audio generation successful.")
-    # Save each generated wav file to a local file
-
-    if stream:
-        print('generate with stream mode ..')
-        wavs = [np.array([[]])]
-        for gen in wavs_gen:
-            print('got new chunk', gen)
-            # play chunk or combine into one complete audio;
-            wavs[0] = np.hstack([wavs[0], np.array(gen[0])])
+    chat = ChatTTS.Chat(get_logger("ChatTTS"))
+    logger.info("Initializing ChatTTS...")
+    if chat.load_models():
+        logger.info("Models loaded successfully.")
     else:
-        print('generate without stream mode ..')
-        wavs = wavs_gen
+        logger.error("Models load failed.")
+        sys.exit(1)
 
+    wavs = chat.infer(texts, use_decoder=True)
+    logger.info("Inference completed. Audio generation successful.")
+    # Save each generated wav file to a local file
     for index, wav in enumerate(wavs):
         save_wav_file(wav, index)
 
-    return Audio(wavs[0], rate=24_000, autoplay=True)
-
 if __name__ == "__main__":
-    print("Starting the TTS application...")
-
-    parser = argparse.ArgumentParser(description='ChatTTS cmd demo', usage="--stream hello, my name is bob.")
-    parser.add_argument("--stream", action="store_true", default=False, help="Use stream infer")
+    logger.info("Starting the TTS application...")
+    parser = argparse.ArgumentParser(description='ChatTTS Command', usage="--stream hello, my name is bob.")
     parser.add_argument("text", help="Original text", default='YOUR TEXT HERE', nargs='*')
     args = parser.parse_args()
-    main(text=' '.join(args.text), stream=args.stream)
-    print("TTS application finished.")
+    main(args.text)
+    logger.info("TTS application finished.")
