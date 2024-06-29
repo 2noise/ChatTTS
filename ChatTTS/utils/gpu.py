24ae4f0f2468313a43b11d24d47ab129c425f39e
源文雨 <41315874+fumiama@users.noreply.github.com>
2024-06-29 17:30:06 +0900

    optimize(utils): gpu selecting logic

--- ChatTTS/utils/gpu.py
+++ ChatTTS/utils/gpu.py
@@ -5,19 +5,22 @@ from .log import logger
 
 def select_device(min_memory=2047, experimental=False):
     if torch.cuda.is_available():
-        available_gpus = []
+        selected_gpu = 0
+        max_free_memory = -1
         for i in range(torch.cuda.device_count()):
             props = torch.cuda.get_device_properties(i)
             free_memory = props.total_memory - torch.cuda.memory_reserved(i)
-            available_gpus.append((i, free_memory))
-        selected_gpu, max_free_memory = max(available_gpus, key=lambda x: x[1])
-        device = torch.device(f"cuda:{selected_gpu}")
+            if max_free_memory < free_memory:
+                selected_gpu = i
+                max_free_memory = free_memory
         free_memory_mb = max_free_memory / (1024 * 1024)
         if free_memory_mb < min_memory:
             logger.get_logger().warning(
                 f"GPU {selected_gpu} has {round(free_memory_mb, 2)} MB memory left. Switching to CPU."
             )
             device = torch.device("cpu")
+        else:
+            device = torch.device(f"cuda:{selected_gpu}")
     elif torch.backends.mps.is_available():
         """
         Currently MPS is slower than CPU while needs more memory and core utility,
