eeb240beac65c2ad8cbba7df7d4a34c0dfc87b25
源文雨 <41315874+fumiama@users.noreply.github.com>
2024-07-03 16:18:23 +0900

    optimize: reduce test time consumption

--- tests/#511.py
+++ tests/#511.py
@@ -6,52 +6,39 @@ if sys.platform == "darwin":
 now_dir = os.getcwd()
 sys.path.append(now_dir)
 
+import logging
+
 import ChatTTS
 
 from tools.logger import get_logger
 
-logger = get_logger("Test #511")
+logger = get_logger("Test #511", lv=logging.WARN)
 
 chat = ChatTTS.Chat(logger)
-chat.load(compile=False) # Set to True for better performance
+chat.load(compile=False, source='huggingface') # Set to True for better performance
 
-texts = ["语音太短了会造成生成音频错误, 这是占位占位, 老大爷觉得车夫的想法很有道理", 
-         "评分只是衡量音色的稳定性,不代表音色的好坏, 可以根据自己的需求选择合适的音色",
-         "举个简单的例子,如果一个沙哑且结巴的音色一直很稳定，那么它的评分就会很高。",
-         "语音太短了会造成生成音频错误, 这是占位占位。我使用 seed id 去生成音频, 但是生成的音频不稳定",
-         "seed id只是一个参考ID 不同的环境下音色不一定一致。还是推荐使用 .pt 文件载入音色",
-         "语音太短了会造成生成音频错误, 这是占位占位。音色标的男女准确吗",
-         "当前第一批测试的音色有两千条, 根据声纹相似性简单打标, 准确度不高, 特别是特征一项",
-         "语音太短了会造成生成音频错误, 这是占位占位。仅供参考。如果大家有更好的标注方法,欢迎 PR。",
-         ]
-
-rand_spk = chat.sample_random_speaker()
+texts = ['的 话 语 音 太 短 了 会 造 成 生 成 音 频 错 误 ， 这 是 占 位 占 位 ， 老 大 爷 觉 得 车 夫 的 想 法 很 有 道 理 [uv_break]', '的 话 评 分 只 是 衡 量 音 色 的 稳 定 性 ， 不 代 表 音 色 的 好 坏 ， 可 以 根 据 自 己 的 需 求 选 择 [uv_break] 合 适 的 音 色', '然 后 举 个 简 单 的 例 子 ， 如 果 一 个 [uv_break] 沙 哑 且 结 巴 的 音 色 一 直 很 稳 定 ， 那 么 它 的 评 分 就 会 很 高 。', '语 音 太 短 了 会 造 成 生 成 音 频 错 误 ， 这 是 占 位 [uv_break] 占 位 。 我 使 用 seed id 去 生 成 音 频 ， 但 是 生 成 的 音 频 不 稳 定', '在d id 只 是 一 个 参 考 id [uv_break] 不 同 的 环 境 下 音 色 不 一 定 一 致 。 还 是 推 荐 使 用 。 pt 文 件 载 入 音 色', '的 话 语 音 太 短 了 会 造 成 生 成 音 频 错 误 ， 这 是 占 位 占 位 。 音 色 标 的 男 女 [uv_break] 准 确 吗', '， 当 前 第 一 批 测 试 的 音 色 有 两 千 条 [uv_break] ， 根 据 声 纹 相 似 性 简 单 打 标 ， 准 确 度 不 高 ， 特 别 是 特 征 一 项', '语 音 太 短 了 会 造 成 生 成 音 频 错 误 ， 这 是 占 位 占 位 。 仅 供 参 考 。 如 果 大 家 有 更 好 的 标 注 方 法 ， 欢 迎 pr [uv_break] 。']
 
 params_infer_code = ChatTTS.Chat.InferCodeParams(
-    spk_emb = rand_spk, # add sampled speaker 
-    temperature = .3,   # using custom temperature
-    top_P = 0.005,        # top P decode
-    top_K = 1,         # top K decode
-)
-
-params_refine_text = ChatTTS.Chat.RefineTextParams(
-    prompt='[oral_0][laugh_0][break_4]',
+    spk_emb = chat.sample_random_speaker(),
+    temperature = .3,
+    top_P = 0.005,
+    top_K = 1,
+    show_tqdm=False,
 )
 
 fail = False
 
-for i in range(4):
+wavs = chat.infer(
+    texts,
+    skip_refine_text=True,
+    params_infer_code=params_infer_code,
+)
 
-    wavs = chat.infer(
-        texts,
-        params_refine_text=params_refine_text,
-        params_infer_code=params_infer_code,
-    )
-
-    for k, wav in enumerate(wavs):
-        if wav is None:
-            logger.warn("iter", i, "index", k, "is None")
-            fail = True
+for k, wav in enumerate(wavs):
+    if wav is None:
+        logger.warn("index", k, "is None")
+        fail = True
 
 if fail:
     import sys
--- tests/testall.sh
+++ tests/testall.sh
@@ -4,12 +4,14 @@ exitcode=0
 
 for file in tests/*.py
 do
+    echo "Testing $file..."
     python "$file"
     if [ $? -ne 0 ]
     then
         echo "Error: $file exited with a non-zero status."
         exitcode=1
     fi
+    echo "Test $file success"
 done
 
 exit $exitcode
