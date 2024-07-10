47287db03b75a7270a70deb18942609d0d991e1d
此般浅薄 <402147150@qq.com>
2024-07-10 11:42:08 +0800

    optimize stream play and adapt to the latest dev changes

--- examples/cmd/stream.py
+++ examples/cmd/stream.py
@@ -4,89 +4,84 @@ import random
 
 import numpy as np
 
-from tools.audio import unsafe_float_to_int16
+from tools.audio import float_to_int16
 
 # 流式推理数据获取器，支持流式获取音频编码字节流
-class BatchStreamer:
-    @classmethod
-    def _batch_unsafe_float_to_int16(cls,audios):
-        valid_audios_info=[(index,audio.shape[0]) for index,audio in enumerate(audios) if audio is not None]
-        allmax_size=max([i[1] for i in valid_audios_info])
-        valid_audios=np.zeros((len(valid_audios_info),allmax_size))
-        for valid_index,(index,maxsize) in enumerate(valid_audios_info):
-            valid_audios[valid_index,:maxsize]=audios[index]
-        
-        valid_audios=unsafe_float_to_int16(valid_audios)
-        result_audios=[None]*len(audios)
-        for i,(valid_index,max_size) in enumerate(valid_audios_info):
-            result_audios[valid_index]=valid_audios[i,:max_size]
-        return result_audios
-        
+class ChatStreamer:
+    def __init__(self,base_block_size=8000):
+        self.base_block_size=base_block_size
     # stream状态更新。数据量不足的stream，先存一段时间，直到拿到足够数据，监控小块数据情况
-    @classmethod
-    def _update_stream(cls,history_stream_wav,new_stream_wav,thre):
-        result_stream=[]
+    @staticmethod
+    def _update_stream(history_stream_wav,new_stream_wav,thre):
         if history_stream_wav is not None:
-            n_texts=len(new_stream_wav)
-            for i in range(n_texts):
-                if new_stream_wav[i] is not None:
-                    result_stream.append(np.concatenate([history_stream_wav[i],new_stream_wav[i]]))
-                else:
-                    result_stream.append(history_stream_wav[i])
-            is_keep_next=sum([i.shape[0] for i in result_stream if i is not None])<thre
+            result_stream=np.concatenate([history_stream_wav,new_stream_wav],axis=1)
+            is_keep_next=result_stream.shape[0]*result_stream.shape[1]<thre
             if random.random()>0.1:
                 print("update_stream",is_keep_next,[i.shape if i is not None else None for i in result_stream])
         else:
             result_stream=new_stream_wav
-            is_keep_next=sum([i.shape[0] for i in result_stream if i is not None])<thre
+            is_keep_next=result_stream.shape[0]*result_stream.shape[1]<thre
 
         return result_stream,is_keep_next
     
     # 已推理batch数据保存
-    @classmethod
-    def _accum(cls,accum_wavs,stream_wav):
-        n_texts=len(stream_wav)
+    @staticmethod
+    def _accum(accum_wavs,stream_wav):
         if accum_wavs is None:
-            accum_wavs=[[i] for i in stream_wav]
+            accum_wavs=stream_wav
         else:
-            for i_text in range(n_texts):
-                if stream_wav[i_text] is not None:
-                    accum_wavs[i_text].append(stream_wav[i_text])
+            accum_wavs=np.concatenate([accum_wavs,stream_wav],axis=1)
         return accum_wavs
     
     # batch stream数据格式转化
-    @classmethod
-    def batch_stream_formatted(cls,stream_wav,output_format='PCM16_byte'):
+    @staticmethod
+    def batch_stream_formatted(stream_wav,output_format='PCM16_byte'):
         if output_format in ('PCM16_byte','PCM16'):
-            format_data=cls._batch_unsafe_float_to_int16(stream_wav)
+            # format_data=ChatStreamer._batch_unsafe_float_to_int16(stream_wav)
+            format_data=float_to_int16(stream_wav)
         else:
             format_data=stream_wav
         return format_data
     
     # 数据格式转化
-    @classmethod
-    def formatted(cls,data,output_format='PCM16_byte'):
+    @staticmethod
+    def formatted(data,output_format='PCM16_byte'):
         if output_format=='PCM16_byte':
             format_data=data.astype("<i2").tobytes()
         else:
             format_data=data
         return format_data
     
-    # 数据生成
-    @classmethod
-    def generate(cls,chatstream,output_format=None,base_block_size=8000):
+    # 检查声音是否为空
+    @staticmethod
+    def checkvoice(data):
+        if np.abs(data).max()<1e-6:
+            return False
+        else:
+            return True
+        
+    # 将声音进行适当拆分返回
+    @staticmethod
+    def _subgen(data,thre=12000):
+        for stard_idx in range(0,data.shape[0],thre):
+            end_idx=stard_idx+thre
+            yield data[stard_idx:end_idx]
+            
+    # 流式数据获取，支持获取音频编码字节流
+    def generate(self,streamchat,output_format=None):
         assert output_format in ('PCM16_byte','PCM16',None)
         curr_sentence_index=0
         history_stream_wav=None
         article_streamwavs=None
-        for stream_wav in chatstream:
+        for stream_wav in streamchat:
+            print(np.abs(stream_wav).max(axis=1))
             n_texts=len(stream_wav)
-            n_valid_texts=len(list(filter(lambda x:x is not None,stream_wav)))
+            n_valid_texts=(np.abs(stream_wav).max(axis=1)>1e-6).sum()
             if n_valid_texts==0:
                 continue
             else:
-                block_thre=n_valid_texts*base_block_size
-                stream_wav,is_keep_next=cls._update_stream(history_stream_wav,stream_wav,block_thre)
+                block_thre=n_valid_texts*self.base_block_size
+                stream_wav,is_keep_next=ChatStreamer._update_stream(history_stream_wav,stream_wav,block_thre)
                 # 数据量不足，先保存状态
                 if is_keep_next:
                     history_stream_wav=stream_wav
@@ -94,89 +89,77 @@ class BatchStreamer:
                 # 数据量足够，执行写入操作
                 else:
                     history_stream_wav=None
-                    stream_wav=cls.batch_stream_formatted(stream_wav,output_format)
-                    article_streamwavs=cls._accum(article_streamwavs,stream_wav)
+                    stream_wav=ChatStreamer.batch_stream_formatted(stream_wav,output_format)
+                    article_streamwavs=ChatStreamer._accum(article_streamwavs,stream_wav)
                     # 写入当前句子
-                    if stream_wav[curr_sentence_index] is not None:
-                        yield cls.formatted(stream_wav[curr_sentence_index],output_format)
+                    if  ChatStreamer.checkvoice(stream_wav[curr_sentence_index]):
+                        for sub_wav in ChatStreamer._subgen(stream_wav[curr_sentence_index]):
+                            if ChatStreamer.checkvoice(sub_wav):
+                                yield ChatStreamer.formatted(sub_wav,output_format)
                     # 当前句子已写入完成，直接写下一个句子已经推理完成的部分
                     elif curr_sentence_index<n_texts-1:
                         curr_sentence_index+=1
                         print('add next sentence')
-                        finish_stream_wavs=np.concatenate(article_streamwavs[curr_sentence_index])
-                        yield cls.formatted(finish_stream_wavs,output_format)
+                        finish_stream_wavs=article_streamwavs[curr_sentence_index]
+                        
+                        for sub_wav in ChatStreamer._subgen(finish_stream_wavs):
+                            if ChatStreamer.checkvoice(sub_wav):
+                                yield ChatStreamer.formatted(sub_wav,output_format)
+                        
                     # streamchat遍历完毕，在外层把剩余结果写入
                     else:
                         break
         # 本轮剩余最后一点数据写入
         if is_keep_next:
             if len(list(filter(lambda x:x is not None,stream_wav)))>0:
-                stream_wav=cls.batch_stream_formatted(stream_wav,output_format)
-                if stream_wav[curr_sentence_index] is not None:
-                    yield cls.formatted(stream_wav[curr_sentence_index],output_format)
-                    article_streamwavs=cls._accum(article_streamwavs,stream_wav)
+                stream_wav=ChatStreamer.batch_stream_formatted(stream_wav,output_format)
+                if  ChatStreamer.checkvoice(stream_wav[curr_sentence_index]):
+                    
+                    for sub_wav in ChatStreamer._subgen(stream_wav[curr_sentence_index]):
+                        if ChatStreamer.checkvoice(sub_wav):
+                            yield ChatStreamer.formatted(sub_wav,output_format)
+                    article_streamwavs=ChatStreamer._accum(article_streamwavs,stream_wav)
         # 把已经完成推理的下几轮剩余数据写入
         for i_text in range(curr_sentence_index+1,n_texts):
-            finish_stream_wavs=np.concatenate(article_streamwavs[i_text])
-            yield cls.formatted(finish_stream_wavs,output_format)
+            finish_stream_wavs=article_streamwavs[i_text]
+            
+            for sub_wav in ChatStreamer._subgen(finish_stream_wavs):
+                if ChatStreamer.checkvoice(sub_wav):
+                    yield ChatStreamer.formatted(sub_wav,output_format)
+    # 流式播放接口
+    def play(self,streamchat,wait=5):
+        import pyaudio  # please install it manually
+        import time
+        p = pyaudio.PyAudio()
+        print(p.get_device_count())
+        # 设置音频流参数
+        FORMAT = pyaudio.paInt16  # 16位深度
+        CHANNELS = 1  # 单声道
+        RATE = 24000  # 采样率
+        CHUNK = 1024  # 每块音频数据大小
 
+        # 打开输出流（扬声器）
+        stream_out = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True,)
 
-
-# 直接播放
-def example_0(streamchat):
-    import time
-    p = pyaudio.PyAudio()
-    print(p.get_device_count())
-    # 设置音频流参数
-    FORMAT = pyaudio.paInt16  # 16位深度
-    CHANNELS = 1  # 单声道
-    RATE = 24000  # 采样率
-    CHUNK = 1024  # 每块音频数据大小
-
-    # 打开输出流（扬声器）
-    stream_out = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True,)
-
-    for i in (BatchStreamer.generate(streamchat,output_format='PCM16_byte')):
-        stream_out.write(i)    
-
-    stream_out.stop_stream()
-    stream_out.close()
-
-# 先存放一部分，存的差不多了再播放，适合生成速度比较慢的cpu玩家使用
-def example_1(streamchat,first_prefill_size=300000):
-    import time
-    p = pyaudio.PyAudio()
-    print(p.get_device_count())
-    # 设置音频流参数
-    FORMAT = pyaudio.paInt16  # 16位深度
-    CHANNELS = 1  # 单声道
-    RATE = 24000  # 采样率
-    CHUNK = 1024  # 每块音频数据大小
-
-    # 打开输出流（扬声器）
-    stream_out = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True,)
-
-    prefill_bytes=b''
-    meet=False
-    for i in (BatchStreamer.generate(streamchat,output_format='PCM16_byte')):
+        first_prefill_size=wait*RATE
+        prefill_bytes=b''
+        meet=False
+        for i in self.generate(streamchat,output_format='PCM16_byte'):
+            if not meet:
+                prefill_bytes+=i
+                if len(prefill_bytes)>first_prefill_size:
+                    meet=True
+                    stream_out.write(prefill_bytes)    
+            else:
+                stream_out.write(i)    
         if not meet:
-            prefill_bytes+=i
-            if len(prefill_bytes)>first_prefill_size:
-                meet=True
-                stream_out.write(prefill_bytes)    
-        else:
-            stream_out.write(i)    
-    if not meet:
-        stream_out.write(prefill_bytes)    
+            stream_out.write(prefill_bytes)    
 
-
-    stream_out.stop_stream()
-    stream_out.close()
+        stream_out.stop_stream()
+        stream_out.close()
 
 if __name__ == "__main__":
     import ChatTTS
-    import pyaudio  # please install it manually
-
 
     # 加载 ChatTTS
     chat = ChatTTS.Chat()
@@ -195,20 +178,6 @@ if __name__ == "__main__":
         [
             "总结一下，AI Agent是大模型功能的扩展，让AI更接近于通用人工智能，也就是我们常说的AGI。",
             "你太聪明啦。",
-            "举个例子，大模型可能可以写代码，但它不能独立完成一个完整的软件开发项目。这时候，AI Agent就根据大模型的智能，结合记忆和规划，使用合适的工具，一步步实现从需求分析到产品上线。",
-        ],
-        skip_refine_text=True,
-        params_infer_code=params_infer_code,
-        stream=True,
-    )
-    # 直接播放
-    example_0(streamchat)
-
-    # 获取ChatTTS 流式推理generator
-    streamchat = chat.infer(
-        [
-            "总结一下，AI Agent是大模型功能的扩展，让AI更接近于通用人工智能，也就是我们常说的AGI。",
-            "你太聪明啦。",
             "举个例子，大模型可能可以写代码，但它不能独立完成一个完整的软件开发项目。这时候，AI Agent就根据大模型的智能，结合记忆和规划，一步步实现从需求分析到产品上线。",
         ],
         skip_refine_text=True,
@@ -216,4 +185,4 @@ if __name__ == "__main__":
         params_infer_code=params_infer_code
     )
     # 先存放一部分，存的差不多了再播放，适合生成速度比较慢的cpu玩家使用
-    example_1(streamchat,first_prefill_size=300000)
+    ChatStreamer().play(streamchat,wait=5)
