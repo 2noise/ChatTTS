680e046f7a116193f1c003887920d8de4552302a
此般浅薄 <152078521+ZaymeShaw@users.noreply.github.com>
2024-07-28 23:52:38 +0800

    fix: 降低zero-shot功能对torch版本的要求 (#638)
    * fix some problem
    
    * delete annotation
    
    * some simplify
    
    * chore(format): run black on dev
    
    * correct the device_gpt
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- ChatTTS/model/tokenizer.py
+++ ChatTTS/model/tokenizer.py
@@ -180,12 +180,12 @@ class Tokenizer:
             dtype="<u2",
         ).copy()
         del dec
-        return torch.from_numpy(p).view(*shp)
+        return torch.from_numpy(p.astype(np.int32)).view(*shp)
 
     @staticmethod
     @torch.no_grad()
     def _encode_prompt(prompt: torch.Tensor) -> str:
-        arr: np.ndarray = prompt.to(dtype=torch.uint16, device="cpu").numpy()
+        arr: np.ndarray = prompt.cpu().numpy().astype(np.uint16)
         shp = arr.shape
         assert len(shp) == 2, "prompt must be a 2D tensor"
         s = b14.encode_to_string(
