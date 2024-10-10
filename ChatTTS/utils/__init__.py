880104b615999f2c99b7f7f9af83f58df4cd4b78
shanshan shen <shanshanshen333@gmail.com>
2024-10-10 09:36:46 +0000

    fix problems of the review

--- ChatTTS/utils/__init__.py
+++ ChatTTS/utils/__init__.py
@@ -1,4 +1,4 @@
 from .dl import check_all_assets, download_all_assets
-from .device import select_device
+from .gpu import select_device
 from .io import get_latest_modified_file, del_all
 from .log import logger
